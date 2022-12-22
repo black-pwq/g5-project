ede9cd4335641ddb1241179b85dcd47385b99b7d
Black <black_pwq@qq.com>
2022-12-23 00:42:40 +0800

    adjust shell srcipt for cmake

---
+++ config/cmake_vs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+root="D:/Mine/Courses/Professional Courses/Computer Graphics/G5_Project";
+builddir="/"${root/:/}"/build";
+if [[ ! $1 ]]; then
+	echo "No arg passed into, use root directory";
+	srcdir="$root";
+else
+	srcdir="$1";
+fi
+
+echo "srcdir=$srcdir"
+echo "builddir=$builddir"
+
+function build() {
+	if [ $? ]; then 
+		cmake -B "$builddir" "$srcdir";
+		if [ $? ]; then
+			cd "$builddir";
+			cmake --build . --parallel 8;
+			cd -;
+		fi
+	fi
+}
+
+if [ -d "$builddir" ]; then
+	echo "removing build dir";
+	rm -rf "$builddir";
+	build
+else
+	build
+fi
